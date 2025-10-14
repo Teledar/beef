c63bf48b3f41d0f8041722e34ccaebdd3a0155a9
Teledar <lmartin1920@outlook.com>
2025-10-14 14:17:44 -0400

    Implement basic keyboard support

--- beef.c
+++ beef.c
@@ -13,5 +13,17 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
         graphics_blt_green(&graphics);
         graphics_free_backbuffer(&graphics);
     }
+
+    EFI_GUID guid = EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL_GUID;
+    EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL* keyboard;
+    // TODO: Should we use an attribute other than GET_PROTOCOL?
+    uefi_call_wrapper(ST->BootServices->OpenProtocol, 6, ST->ConsoleInHandle, &guid, 
+        (VOID**)&keyboard, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    EFI_KEY_DATA keystroke;
+    
+    while (keystroke.Key.UnicodeChar == 0) {
+        uefi_call_wrapper(keyboard->ReadKeyStrokeEx, 2, keyboard, &keystroke);
+    }
+    
     return EFI_SUCCESS;
 }
